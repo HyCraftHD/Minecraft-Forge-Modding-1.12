d8c4277a7a8b0c60233fcfee1a52ad5dd9ca8ad2
HyCraftHD <hycrafthd@live.de>
2019-01-31 17:07:12 +0100

    Video 21

--- src/main/java/net/hycrafthd/tutorialmod/proxy/CommonProxy.java
+++ src/main/java/net/hycrafthd/tutorialmod/proxy/CommonProxy.java
@@ -1,28 +1,29 @@
 package net.hycrafthd.tutorialmod.proxy;
 
-import net.hycrafthd.tutorialmod.init.TutorialBlocks;
-import net.hycrafthd.tutorialmod.init.TutorialItems;
-import net.hycrafthd.tutorialmod.init.TutorialSmelting;
+import net.hycrafthd.tutorialmod.init.*;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
-
+	
 	public void preinit(FMLPreInitializationEvent event) {
 		TutorialItems.init();
 		TutorialBlocks.init();
-
+		
 		MinecraftForge.EVENT_BUS.register(TutorialItems.class);
 		MinecraftForge.EVENT_BUS.register(TutorialBlocks.class);
+		
+		GameRegistry.registerWorldGenerator(new TutorialWorldGeneration(), 0);
 	}
-
+	
 	public void init(FMLInitializationEvent event) {
 		TutorialSmelting.init();
 	}
-
+	
 	public void postinit(FMLPostInitializationEvent event) {
 	}
-
+	
 }
